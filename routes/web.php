b5e7353545752630ef69dc0eb1d5ba4eb502ecc2
Philipp Schächtele <philipp.schaechtele@googlemail.com>
2024-12-04 14:06:49 +0100

    Update web.php
    Mask Client IP, Cache JavaScript for faster loading

--- routes/web.php
+++ routes/web.php
@@ -2,15 +2,28 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Http\Request;
 use App\Http\Middleware\VerifyCsrfToken;
 
-// plausible.io proxy 
+// plausible.io proxy, cache for 1 day 
 Route::get("/pio.js", function (Request $request) {
-  $response = Http::get('https://plausible.io/js/plausible.js');
-  $contents = $response->body();
-  $header = ['Content-Type' => 'application/javascript'];
-  return response($contents, 200, $header);
+  // Define a unique cache key for the script
+  $cacheKey = 'plausible_js_script';
+
+  // Try to get the cached content
+  $contents = Cache::remember($cacheKey, 86400, function () { // Cache for 1 day (86400 seconds)
+      $response = Http::get('https://plausible.io/js/plausible.js');
+
+      if ($response->successful()) {
+          return $response->body(); // Cache the response body
+      }
+
+      abort(502, 'Unable to fetch the script from the third-party provider.');
+  });
+
+  // Return the response with appropriate headers
+  return response($contents, 200, ['Content-Type' => 'application/javascript']);
 });
 
 Route::post("/api/event", function (Request $request) {
@@ -20,13 +33,15 @@ Route::post("/api/event", function (Request $request) {
   $header = $req->header();
   $userAgent = array_key_exists('user-agent', $header) ? $header['user-agent'] : '';
   $clientIp = isset($_SERVER["HTTP_CF_CONNECTING_IP"]) ? $_SERVER["HTTP_CF_CONNECTING_IP"] : $req->ip();
+  $maskedIp = preg_replace('/\.\d+$/', '.1', $clientIp);
 
   $headerEvent = [
     "User-Agent"      => $userAgent,
-    "X-Forwarded-For" => $clientIp,
+    "X-Forwarded-For" => $maskedIp,
     "Content-Type" => "application/json"
   ];
-  // send event to plausible
+  // send event to plausible, timeout of 10s
+
   $response = Http::withHeaders($headerEvent)->send('POST', 'https://plausible.io/api/event', [
     'body' => $content
   ]);
